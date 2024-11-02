5ab7455b9dc970d74d59d3bf83ed755d07ccdcd2
oskarnaranjo123 <oscarelmono123@gmail.com>
2024-11-02 15:33:19 -0500

    formulario y validaciones de autenticación

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace Autenticacion
             // Add services to the container.
             builder.Services.AddRazorPages();
 
+            builder.Services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+            {
+                options.Cookie.Name = "MyCookieAuth";
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
