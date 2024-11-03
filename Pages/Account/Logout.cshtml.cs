cfb61ae8d3e7140bd333ee226695c3206eca4cf7
oskarnaranjo123 <oscarelmono123@gmail.com>
2024-11-02 22:26:09 -0500

    Logout users

---
+++ Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+
+
+namespace Autenticacion.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToPage("/Index");
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
